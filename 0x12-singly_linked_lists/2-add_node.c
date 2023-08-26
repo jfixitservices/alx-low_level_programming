21e970822c0e9df27beeec7b491c879e51bc85df
Frey John Wisdom <frey.john.wisdom@gmail.com>
2023-08-25 17:38:23 -0700

    updates

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,7 +23,8 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 	new_node->next = *head;
+	new_node->len = len;
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
