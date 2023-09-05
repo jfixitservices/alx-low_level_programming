d8e621a5903702db363ee0af1af56e278974894b
Frey John Wisdom <frey.john.wisdom@gmail.com>
2023-09-05 14:38:37 -0700

    This is a 0x15. C - File I/O commit by John Frey

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,61 +1,3 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-char *create_buffer(char *file);
-void close_file(int fd);
-
-/**
- * create_buffer – This allocates 1024 bytes for a buffer.
- * @file: This is the name of the file buffer is storing chars for.
- *
- * Return: This is the pointer to the newly-allocated buffer.
- */
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
-
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int c;
-
-	c = close(fd);
-
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-
-/**
- * main – This copies the contents of a file to another file.
- * @argc: This is the number of arguments supplied to the program.
- * @argv: This is the array of pointers to the arguments.
- *
- * Return: This returns 0 on success.
- *
- * Description: If the argument count is not correct - exit code 97.
- * If file_from doesn’t exist or cannot be read - exit code 98.
- * If file_to can’t be created or written to - exit code 99.
- * If file_to or file_from can’t be closed - exit code 100.
- */
 int main(int argc, char *argv[])
 {
 	int from, to, r, w;
@@ -72,7 +14,8 @@ int main(int argc, char *argv[])
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	do {
+	do
+	{
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
