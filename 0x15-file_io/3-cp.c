72beacf15b9c291ec1a8ef0298803f58393d3220
Frey John Wisdom <frey.john.wisdom@gmail.com>
2023-09-05 15:02:54 -0700

    This is a 0x15. C - File I/O commit by John Frey

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,10 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer – This allocates 1024 bytes for a buffer.
- * @file: This is the name of the file buffer is storing chars for.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: This is the pointer to the newly-allocated buffer.
+ * Return: A pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
 {
@@ -45,16 +45,16 @@ void close_file(int fd)
 }
 
 /**
- * main – This copies the contents of a file to another file.
- * @argc: This is the number of arguments supplied to the program.
- * @argv: This is the array of pointers to the arguments.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: This returns 0 on success.
+ * Return: 0 on success.
  *
- * Description: If the argument count is not correct - exit code 97.
- * If file_from doesn’t exist or cannot be read - exit code 98.
- * If file_to can’t be created or written to - exit code 99.
- * If file_to or file_from can’t be closed - exit code 100.
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
